628558a8808385c3b6d1c0635b0bb1862168c155
Kseniya <kkosnyreva@digimatics.ru>
2023-02-27 02:57:12 +0300

    hw_03

---
+++ src/test/java/BasePage.java
@@ -0,0 +1,87 @@
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+import java.io.File;
+
+import static com.codeborne.selenide.Condition.exist;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byName;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class BasePage {
+
+    public void openUrl(String url) {
+        open(url);
+    }
+
+    public BasePage setFirstName (String fname) {
+        $("#firstName").setValue(fname);
+        return this;
+    }
+
+    public BasePage setLastName (String lname) {
+        $("#lastName").setValue(lname);
+        return this;
+    }
+
+    public BasePage setEmail (String email) {
+        $("#userEmail").setValue(email);
+        return this;
+    }
+
+    public BasePage setGender (String gender) {
+        $(byText(gender)).click();
+        return this;
+    }
+
+    public BasePage setPhone (String phone) {
+        $x("//input[@id='userNumber']").setValue(phone);
+        return this;
+    }
+//не понимаю почему не вычищает поле даты
+    public BasePage setDate (String date) {
+        SelenideElement el = $x("//input[@id='dateOfBirthInput']");
+        el.setValue("");
+        el.setValue(date).pressEnter();
+        return this;
+    }
+
+    public BasePage setSubject (String subject) {
+        $x("//input[@id='subjectsInput']").setValue(subject).pressEnter();
+        return this;
+    }
+
+    public BasePage setHobbies (String hobbies) {
+        $(byText(hobbies)).click();
+        return this;
+    }
+
+    public BasePage uploadFiles (String url) {
+        File file = new File(url);
+        $("#uploadPicture").uploadFile(file);
+        return this;
+    }
+
+    public BasePage setCurrentAddress (String address) {
+        $x("//textarea[@id='currentAddress']").setValue(address);
+        return this;
+    }
+
+    public BasePage setState (String state) {
+        $x("//input[@id='react-select-3-input']").setValue(state).pressEnter();
+        return this;
+    }
+//была мысль в трай кетч заверуть в условный оператор учесть неактивность поля из-за невыбранного штата
+    public BasePage setCity (String city){
+        $x("//input[@id='react-select-4-input']").setValue(city).pressEnter();
+        return this;
+    }
+
+    public BasePage clickSubmit (){
+        $x("//button[@id='submit']").click();
+        return this;
+    }
+
+}
---
+++ src/test/java/FirstTest.java
@@ -0,0 +1,55 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class FirstTest {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browserSize = "1920x1080";
+    }
+
+    BasePage page = new BasePage();
+
+    @Test
+    void firstEnterTest() {
+        page.openUrl("/automation-practice-form");
+        executeJavaScript("$('footer').css('position','relative')"); //костыльчик, просто убрать футер не помогло
+        page.setFirstName("Kseniya");
+        page.setLastName("Kosnyreva");
+        page.setEmail("kosnireva9469@gmail.com");
+        page.setGender("Female");
+        page.setPhone("7961822211");
+        page.setDate("25 Feb 1994");
+        page.setSubject("Economics");
+        page.setHobbies("Reading");
+        page.uploadFiles("src/test/resources/pic.png"); //по хорошему часть пути нужно вынести в конфиг файл в ресурсы
+        page.setCurrentAddress("Я гражданин мира");
+        page.setState("Haryana");
+        page.setCity("Karnal");
+
+        page.clickSubmit();
+
+        $x("//div[@class='table-responsive']").shouldBe(exist);
+        $x("//tr[contains(.,'Student Name')]/td[last()]").shouldHave(text("Kseniya Kosnyreva"));
+        $x("//tr[contains(.,'Student Email')]/td[last()]").shouldHave(text("kosnireva9469@gmail.com"));
+        $x("//tr[contains(.,'Gender')]/td[last()]").shouldHave(text("Female"));
+        $x("//tr[contains(.,'Mobile')]/td[last()]").shouldHave(text("7961822211"));
+        //$x("//tr[contains(.,'Date of Birth')]/td[last()]").shouldHave(text("25 February,1994"));
+        $x("//tr[contains(.,'Subjects')]/td[last()]").shouldHave(text("Economics"));
+        $x("//tr[contains(.,'Hobbies')]/td[last()]").shouldHave(text("Reading"));
+        $x("//tr[contains(.,'Picture')]/td[last()]").shouldHave(text("pic.png"));
+        $x("//tr[contains(.,'Address')]/td[last()]").shouldHave(text("Я гражданин мира"));
+        $x("//tr[contains(.,'State and City')]/td[last()]").shouldHave(text("Haryana Karnal"));
+
+    }
+}
